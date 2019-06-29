8553a250863a7d4190958de43b7be3a6d9d02cb2
Roland Jülich <rolandjuelich@gmail.com>
2019-06-29 11:12:12 +0200

    initial test

--- src/test/java/kata/mars/rover/MarsRoverTest.java
+++ src/test/java/kata/mars/rover/MarsRoverTest.java
@@ -1,41 +1,14 @@
 package kata.mars.rover;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 import org.junit.Test;
 
 public class MarsRoverTest {
 
-	public class MarsRover {
-
-		public int getX() {
-			return 0;
-		}
-
-		public int getY() {
-			return 0;
-		}
-
-		public char getDirection() {
-			return 'N';
-		}
-
-	}
-
 	@Test
-	public void shouldInitializeRover() {
-		// given
-		int x = 0;
-		int y = 0;
-		char direction = 'N';
-
-		// when
-		MarsRover rover = new MarsRover();
-
-		// then
-		assertThat(rover.getX()).isEqualTo(x);
-		assertThat(rover.getY()).isEqualTo(y);
-		assertThat(rover.getDirection()).isEqualTo(direction);
+	public void shouldDoSth() {
+		fail("this could be your first test");
 	}
 
 }
