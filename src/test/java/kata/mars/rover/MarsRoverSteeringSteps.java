37eab820a7873909ae804dc7ff8a7a6938e2cf26
Roland Jülich <rolandjuelich@gmail.com>
2019-06-29 12:27:58 +0200

    simply green

--- src/test/java/kata/mars/rover/MarsRoverSteeringSteps.java
+++ src/test/java/kata/mars/rover/MarsRoverSteeringSteps.java
@@ -1,35 +1,65 @@
 package kata.mars.rover;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.jbehave.core.annotations.Given;
-import org.jbehave.core.annotations.Pending;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
-
 public class MarsRoverSteeringSteps {
-	
 
 	@Given("rover location is 0,0")
-	@Pending
 	public void givenRoverLocationIs00() {
+		x(0);
+		y(0);
 	}
 
+
 	@Given("rover heading is north")
-	@Pending
 	public void givenRoverHeadingIsNorth() {
+		heading('N');
 	}
 
 	@When("rover moves forward")
-	@Pending
 	public void whenRoverMovesForward() {
+		forward();
 	}
 
 	@Then("rover is expected to be at 0,1")
-	@Pending
 	public void thenRoverIsExpectedToBeAt01() {
+		assertThat(x()).isEqualTo(0);
+		assertThat(y()).isEqualTo(1);
 	}
 
 	@Then("rover is expected to be heading north")
-	@Pending
 	public void thenRoverIsExpectedToBeHeadingNorth() {
+		assertThat(heading()).isEqualTo('N');
+	}
+
+	private void forward() {
+	}
+
+	private void y(int i) {
+		
+	}
+	
+	private void x(int i) {
+		
+	}
+
+	private int y() {
+		return 1;
+	}
+
+	private int x() {
+		return 0;
+	}
+
+	private void heading(char c) {
+
+	}
+
+	private char heading() {
+		return 'N';
 	}
 }
