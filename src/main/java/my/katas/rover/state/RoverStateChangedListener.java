ccbd13c72f7d160ca51499ecd1c4356c1b72e3a0
Roland Jülich <rolandjuelich@gmail.com>
2019-08-08 08:35:17 +0200

    baseline

--- src/main/java/my/katas/rover/state/RoverStateChangedListener.java
+++
@@ -1,6 +0,0 @@
-package my.katas.rover.state;
-
-public interface RoverStateChangedListener {
-
-	void notifyThat(final RoverStateChanged event);
-}
--- src/test/java/my/katas/rover/RoverStepDefs.java
+++ src/test/java/my/katas/rover/RoverStepDefs.java
@@ -1,95 +1,50 @@
 package my.katas.rover;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import io.cucumber.java.Before;
+import cucumber.api.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import my.katas.rover.state.RoverStateChanged;
-import my.katas.rover.state.RoverStateChangedListener;
 
 public class RoverStepDefs {
 
-	private Application rover;
-
-	private int currentX;
-	private int currentY;
-	private char currentHeading;
-
-	@Before
-	public void beforeSceanrio() {
-		rover = new Application();
-		rover.register(new RoverStateChangedListener() {
-
-			@Override
-			public void notifyThat(final RoverStateChanged event) {
-				currentX = event.getX();
-				currentY = event.getY();
-				currentHeading = event.getHeading();
-			}
-		});
-	}
-
 	@Given("rover is heading {string}")
 	public void rover_is_heading(final String heading) {
-		rover.initialize(0, 0, heading.toUpperCase().charAt(0));
+		throw new PendingException();
 	}
 
 	@Given("rover is heading {string} at {int}, {int}")
 	public void rover_is_heading_at(final String heading, final Integer x, final Integer y) {
-		rover.initialize(x, y, heading.toUpperCase().charAt(0));
+		throw new PendingException();
 	}
 
 	@When("rover moves forward {int} times")
 	public void rover_moves_forward_times(final Integer times) {
-		for (int i = 0; i < times; i++) {
-			rover.moveForward();
-		}
+		throw new PendingException();
 	}
 
 	@When("rover moves backward {int} times")
 	public void rover_moves_backward_times(final Integer times) {
-		for (int i = 0; i < times; i++) {
-			rover.moveBackward();
-		}
+		throw new PendingException();
 	}
 
 	@When("rover turns right")
 	public void rover_turns_right() {
-		rover.turnRight();
+		throw new PendingException();
 	}
 
 	@When("rover turns left")
 	public void rover_turns_left() {
-		rover.turnLeft();
+		throw new PendingException();
 	}
 
 	@Then("rover should be heading {string}")
 	public void rover_should_be_heading(final String expected) {
-		assertThat(currentHeading()).isEqualTo(expected);
+		throw new PendingException();
 	}
 
 	@Then("rover should be heading {string} at {int}, {int}")
 	public void rover_should_be_heading_at(final String heading, final Integer x, final Integer y) {
-		assertThat(currentX).describedAs("expected x").isEqualTo(x);
-		assertThat(currentY).describedAs("expected y").isEqualTo(y);
-		assertThat(currentHeading()).describedAs("expected heading").isEqualTo(heading.toLowerCase());
-	}
-
-	private String currentHeading() {
-		switch (currentHeading) {
-		case 'N':
-			return "north";
-		case 'E':
-			return "east";
-		case 'S':
-			return "south";
-		case 'W':
-			return "west";
-		default:
-			return null;
-		}
+		throw new PendingException();
 	}
 
 }
