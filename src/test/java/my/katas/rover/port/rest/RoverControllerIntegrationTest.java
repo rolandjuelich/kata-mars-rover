af84ae79f520b30b5ba061741c53c5884cb8f683
Roland Jülich <rolandjuelich@gmail.com>
2019-09-06 17:05:11 +0200

    integration tests

--- src/test/java/my/katas/rover/port/rest/RoverControllerIntegrationTest.java
+++ src/test/java/my/katas/rover/port/rest/RoverControllerIntegrationTest.java
@@ -1,22 +1,20 @@
 package my.katas.rover.port.rest;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import my.katas.rover.Commands;
-import my.katas.rover.initialize.InitializeRover;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,17 +25,32 @@ public class RoverControllerIntegrationTest {
 	@Autowired
 	private MockMvc mvc;
 
-	@Autowired
-	private Commands commandBus;
-
 	@Test
-	public void test() throws Exception {
+	public void shouldInitialize() throws Exception {
+		// given
+		final MockHttpServletRequestBuilder request = get("/initialize");
 
-		mvc.perform(get("/initialize"))
-				.andDo(print())
-				.andExpect(status().isOk());
+		// when
+		final MockHttpServletResponse response = mvc.perform(request).andReturn().getResponse();
 
-		verify(commandBus).execute(any(InitializeRover.class));
+		// then
+		assertThat(response.getStatus()).isEqualTo(OK.value());
+		assertThat(response.getContentAsString()).startsWith("RoverInitialized");
+	}
+
+	
+	@Test
+	@Ignore
+	public void shouldMoveForward() throws Exception {
+		//given
+		final MockHttpServletRequestBuilder request = get("/forward");
+
+		// when
+		final MockHttpServletResponse response = mvc.perform(request).andReturn().getResponse();
+		
+		// then
+		assertThat(response.getStatus()).isEqualTo(OK.value());
+		assertThat(response.getContentAsString()).startsWith("RoverMoved");
 	}
 
 }
