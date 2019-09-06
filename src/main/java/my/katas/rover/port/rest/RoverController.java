18b02961f42761fb7a3cf7c12bbd7e3d1b6fd17a
Roland Jülich <rolandjuelich@gmail.com>
2019-09-06 17:22:07 +0200

    remove obsolete code

--- src/main/java/my/katas/rover/port/rest/RoverController.java
+++ src/main/java/my/katas/rover/port/rest/RoverController.java
@@ -5,13 +5,10 @@ import static org.awaitility.Awaitility.await;
 import static org.awaitility.Duration.FIVE_SECONDS;
 import static org.springframework.web.context.WebApplicationContext.SCOPE_REQUEST;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.eventbus.EventBus;
@@ -25,8 +22,6 @@ import my.katas.rover.move.RoverMoved;
 @Scope(SCOPE_REQUEST)
 public class RoverController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
 	@Autowired
 	private Commands commands;
@@ -37,12 +32,6 @@ public class RoverController {
 	private RoverInitialized initialized;
 	
 	private RoverMoved moved;
-
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(),
-				String.format(template, name));
-	}
 
 	@RequestMapping("/initialize")
 	public String initialize() {
